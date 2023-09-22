7733cfc896dec9acd08c92e3ddf886094a22da08
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-22 13:36:31 +0100

    update files

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+
+
+const Product = () => {
+  return (
+    <Card>
+      <CardMedia>
+        
+      </CardMedia>
+    </Card>
+  )
+}
+
+export default Product
