8c082252f59c6d749c63a645c68368a72818e021
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-10-21 21:21:05 +0100

    update products

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
 
 export {
     Button,
     Nav,
     ShoeCard,
+    PopularProductCard
 }
