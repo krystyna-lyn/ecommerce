bfa96f2098304a9f0ce68325ee45746be2fc510c
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-10-22 12:43:40 +0100

    add popular products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,24 +1,25 @@
 import { PopularProductCard } from "../components/index";
+import { products } from '../constants/index';
 
 const PopularProducts = () => {
   return (
     <section id='products' className='max-container max-sm:mt-12'>
-    <div className='flex flex-col justify-start gap-5'>
-      <h2 className='text-4xl font-palanquin font-bold'>
-        Our <span className='gold-color'> Popular </span> Products
-      </h2>
-      <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
-        Experience top-notch quality and style with our sought-after
-        selections. Discover a world of comfort, design, and value
-      </p>
-    </div>
+      <div className='flex flex-col justify-start gap-5'>
+        <h2 className='text-4xl font-palanquin font-bold'>
+          Our <span className='gold-color'> Popular </span> Products
+        </h2>
+        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
+          designed to elevate your athletic journey and provide the perfect blend of comfort, support, and style.
+        </p>
+      </div>
 
-    <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14'>
-      
-        <PopularProductCard />
-    
-    </div>
-  </section>
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14'>
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+
+    </section>
   )
 }
 
