6e55e58548875f85e4f7a154b2b4480ef8812b5a
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-10-24 16:43:37 +0100

    add services

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,13 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+
 
 export {
     Button,
     Nav,
     ShoeCard,
-    PopularProductCard
+    PopularProductCard,
+    ServiceCard
 }
