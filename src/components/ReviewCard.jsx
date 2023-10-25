3f0aaa1766bbc7af1a8362ae5a8a3488a0df7054
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-10-25 16:59:37 +0100

    Update ReviewCard.jsx

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,11 @@
 
 
-const ReviewCard = ({imgURL, customerName, rating, feedback}) => {
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
-    <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16'>
+    <div className='flex justify-center items-center flex-col'>
       <div >
         <img src={imgURL} alt={customerName} width={240} height={240}
-        className='rounded-full object-cover w-[120px] h-[120px]' />
+          className='rounded-full object-cover w-[120px] h-[120px]' />
       </div>
       <h3 className='mt-5 font-palanquin text-3xl leading-normal font-bold'>
         {customerName}
